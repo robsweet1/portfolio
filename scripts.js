6d12d209a1844b172ef000408c6301e6c96c3f54
Rob <robert.j.sweet@stonybrook.edu>
2020-12-02 21:18:26 -0500

    Commiting current code

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// const lightbox = document.createElement('div')
+// lightbox.id = 'lightbox'
+// document.body.appendChild(lightbox)
+
+const lightbox = document.getElementById('lightbox')
+const screenshots = document.querySelectorAll('img')
+screenshots.forEach(screenshot => {
+    screenshot.addEventListener('click', e => {
+        lightbox.classList.add('lightbox-active')
+        const img = document.createElement('img')
+        img.src = screenshot.src
+        img.id = 'lightbox-image'
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if(e.target !== e.currentTarget) 
+        return
+    lightbox.classList.remove('lightbox-active')
+    lightbox.removeChild(lightbox.querySelector('img'))
+})
